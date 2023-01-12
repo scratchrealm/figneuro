d331149219cf54f7fbe45749250f2c88e6baa428
Jeremy Magland <jmagland@flatironinstitute.org>
2023-01-12 09:00:17 -0500

    saneslab.AudioSpectrogram

--- figneuro-views/src/loadView.tsx
+++ figneuro-views/src/loadView.tsx
@@ -1,11 +1,15 @@
 import { ViewComponentProps } from "@figurl/core-views"
 import { FunctionComponent } from "react"
 import { isTimeseriesGraphViewData, TimeseriesGraphView } from "@figurl/timeseries-views"
+import { AudioSpectrogramView, isAudioSpectrogramViewData } from "./saneslab/view-audio-spectrogram"
 
 const loadView = (o: {data: any, width: number, height: number, opts: any, ViewComponent: FunctionComponent<ViewComponentProps>}) => {
     const {data, width, height} = o
     if (isTimeseriesGraphViewData(data)) {
         return <TimeseriesGraphView data={data} width={width} height={height} />
+    }
+    else if (isAudioSpectrogramViewData(data)) {
+        return <AudioSpectrogramView data={data} width={width} height={height} />
     }
     else return undefined
 }
---
+++ figneuro/saneslab/views/__init__.py
@@ -0,0 +1,1 @@
+from .AudioSpectrogram import AudioSpectrogram
